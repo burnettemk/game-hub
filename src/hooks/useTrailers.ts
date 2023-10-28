815cd858efdc17a3f14df34afdd7d2de19b769d5
burnettemk <wafflesxyz@gmail.com>
2023-10-28 15:11:37 -0700

    Build game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import APICLient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+
+const useTrailers = (gameId: number) => {
+    const apiClient = new APICLient<Trailer>(`/games/${gameId}/movies`)
+
+    return useQuery({
+    queryKey: ['trailers', gameId],
+    queryFn: apiClient.getAll
+})}
+
+export default useTrailers;
