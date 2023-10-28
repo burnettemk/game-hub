6a7049746698373fea0c776ed617f5b415677c20
burnettemk <wafflesxyz@gmail.com>
2023-10-28 15:43:13 -0700

    Notes

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -3,8 +3,10 @@ import APICLient from "../services/api-client";
 import { Trailer } from "../entities/Trailer";
 
 const useTrailers = (gameId: number) => {
+    // This call is responsible for getting the data from the backend using the correct endpoints
     const apiClient = new APICLient<Trailer>(`/games/${gameId}/movies`)
 
+    // This is responsible for caching the data for later use
     return useQuery({
     queryKey: ['trailers', gameId],
     queryFn: apiClient.getAll
